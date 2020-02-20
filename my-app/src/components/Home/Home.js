e521234eb03fff22b387383987f41d791e6c8ac6
Nguyen <nguyenj32@wit.edu>
2020-02-20 14:42:23 -0500

    Modified CSS Files

--- my-app/src/components/Home/Home.js
+++ my-app/src/components/Home/Home.js
@@ -6,9 +6,14 @@ class Home extends Component {
   render() {
     return (
         <div className="container">
-            <p className="home">
-            <img src= {GeoLeopard} alt= "GeoLeopard" className="leopard"/>
-            </p>
+            <div className="home_paragraph">
+                <p>
+                   Computer Science Society
+                </p>
+            </div>
+            <div className ="home">
+                <img src= {GeoLeopard} alt= "GeoLeopard" className="leopard"/>
+            </div>
         </div>
     );
   }
